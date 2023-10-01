00330980570e045d7b1a0111c5f0d48fa9d4d5f0
ivo garofalo <ivo.garofalo@gmail.com>
2023-10-01 14:00:55 +0200

    Add comprehensive tests for Faucet contract and modify YenToken contract

--- contracts/YenToken.sol
+++ contracts/YenToken.sol
@@ -5,14 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract YenToken is ERC20, Ownable {
-    uint256 public constant MAX_SUPPLY = 1000000000 * (10 ** 18); // 1,000,000,000 tokens with 18 decimals
+    uint256 public constant MAX_SUPPLY = 6000000000 * (10 ** 18); // 1,000,000,000 tokens with 18 decimals
 
     constructor() ERC20("Yen", "YEN") {
         uint256 initialAmount = 1000000 ether;
         _mint(msg.sender, initialAmount);
     }
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
