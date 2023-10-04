e229edbf2bdb8f844a078630b9f6f314fc1ec40d
ivo garofalo <ivo.garofalo@gmail.com>
2023-10-04 18:12:00 +0200

    drawio

---
+++ contracts/stakingcontracts/StakingPlatform.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/// @title StakingPlatform
+/// @dev This contract is the main contract of the staking platform.
+contract StakingPlatform {
+    
+}
