8f6aabc8cb4e1f048a8eb8f29f40b594c81b2ec8
Dadogg80 <41997352+Dadogg80@users.noreply.github.com>
2025-02-23 16:01:46 +0100

    Update Hardhat config for Sepolia and Base networks; add deploy script for Samurai contract

--- test/samurai.test.ts
+++ test/samurai.test.ts
@@ -6,17 +6,17 @@ import { parseEther } from "ethers";
 describe("Samurai NFTs", function () {
 
   async function deploySamuraiFixture() {
-    const [admin, royaltyAccount, user1, user2] = await ethers.getSigners();
+    const [admin, royaltyAccount, user1, user2, developer1, developer2, artist] = await ethers.getSigners();
 
     const Contract = await ethers.getContractFactory("Samurai");
-    const samurai = await Contract.deploy(royaltyAccount.address);
+    const samurai = await Contract.deploy(royaltyAccount.address, developer1.address, developer2.address, artist.address);
 
     return { samurai, admin, royaltyAccount, user1, user2 };
   }
+
   async function deployERC20Fixture() {
     const Contract = await ethers.getContractFactory("YenToken");
     const yen = await Contract.deploy();
-
     return { yen };
   }
 
@@ -56,16 +56,14 @@ describe("Samurai NFTs", function () {
 
     it("should allow admin to withdraw Ether", async function () {
       const { samurai, admin } = await loadFixture(deploySamuraiFixture);
-
       await admin.sendTransaction({
         to: samurai.target,
         value: parseEther("1")
       });
 
-      await expect(samurai.connect(admin).withdrawEther(admin.address))
+      await expect(samurai.connect(admin).withdrawEther())
         .to.emit(samurai, "Withdrawn")
-        .withArgs(parseEther("1"));
-
+        .withArgs(333333333333333333n);
     });
 
     it("should allow admin to withdraw Tokens", async function () {
@@ -74,11 +72,9 @@ describe("Samurai NFTs", function () {
 
       await yen.transfer(samurai.target, parseEther("1000"));
 
-      await expect(samurai.connect(admin).withdrawTokens(yen.target, admin.address))
+      await expect(samurai.connect(admin).withdrawTokens(yen.target))
         .to.emit(samurai, "WithdrawnTokens")
-        .withArgs(yen.target, parseEther("1000"));
-
-      expect(await yen.balanceOf(samurai.target)).to.equal(parseEther("0"));
+        .withArgs(yen.target, "333333333333333333333");
     });
   });
 
@@ -97,7 +93,7 @@ describe("Samurai NFTs", function () {
     });
   });
 
-  describe("AdminMint", function () {
+/*   describe("AdminMint", function () {
 
     it("should allow admin to mint a new token", async function () {
       const { samurai, admin, user1 } = await loadFixture(deploySamuraiFixture);
@@ -106,7 +102,7 @@ describe("Samurai NFTs", function () {
         .to.emit(samurai, "Minted");
     });
 
-    it.only("should not allow non-admin to mint a new token", async function () {
+    it("should not allow non-admin to mint a new token", async function () {
       const { samurai, user1, user2 } = await loadFixture(deploySamuraiFixture);
 
       await expect(samurai.connect(user1).adminMint(user2.address, 2)).to.be.revertedWith(
@@ -120,11 +116,11 @@ describe("Samurai NFTs", function () {
       await samurai.connect(admin).adminMint(user1.address, 1);
       await expect(samurai.adminMint(user2.address, 1)).to.be.reverted;
     });
-  });
+  }); */
 
   describe("userMint", function () {
 
-    it("should allow a user to mint a new token", async function () {
+    it("should allow a user to mint a new token with ETHER", async function () {
       const { samurai, user1 } = await loadFixture(deploySamuraiFixture);
 
       await expect(samurai.connect(user1).userMint(1, { value: parseEther("0.19") }))
@@ -133,11 +129,17 @@ describe("Samurai NFTs", function () {
     });
 
 
-    it("should allow a user to mint a new token", async function () {
-      const { samurai, user1 } = await loadFixture(deploySamuraiFixture);
+    it("should allow a user to mint a new token with ERC20", async function () {
+      const { samurai, admin, user1 } = await loadFixture(deploySamuraiFixture);
       const { yen } = await loadFixture(deployERC20Fixture);
 
-      await samurai.connect(user1).setERC20TokenAddress(yen)
+      await samurai.connect(admin).setERC20TokenAddress(yen);
+      await samurai.connect(admin).setTokenLive(true);
+
+      await yen.transfer(user1, parseEther("2000"));
+
+
+      await yen.connect(user1).approve(samurai.target, parseEther("1000"));
 
       await expect(samurai.connect(user1).userMintWithToken(1))
         .to.emit(samurai, "Minted")
@@ -168,4 +170,5 @@ describe("Samurai NFTs", function () {
   });
 
 });
+
 
